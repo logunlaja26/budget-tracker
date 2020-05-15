4b3f1978a2dc9df79165b41c5967ad5002e078d1
Ly Ogunlaja <logunlaja26@gmail.com>
2020-05-14 21:15:30 -0500

    first commit

---
+++ src/test/java/com/lyomann/budgettracker/BudgetTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lyomann.budgettracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BudgetTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
