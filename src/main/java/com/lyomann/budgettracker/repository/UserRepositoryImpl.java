1be1d11e6abe331b7220eb4a95c7c1cd71da496c
Ly Ogunlaja <logunlaja26@gmail.com>
2020-05-26 09:36:41 -0500

    added createnewuser & finduserbyusername integration test methods

--- src/main/java/com/lyomann/budgettracker/repository/UserRepositoryImpl.java
+++ src/main/java/com/lyomann/budgettracker/repository/UserRepositoryImpl.java
@@ -3,11 +3,14 @@ package com.lyomann.budgettracker.repository;
 import com.lyomann.budgettracker.document.Expense;
 import com.lyomann.budgettracker.document.User;
 import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.data.mongodb.core.query.Update;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 
+import java.util.List;
 import java.util.Optional;
 
 import static org.springframework.data.mongodb.core.query.Criteria.*;
@@ -23,12 +26,23 @@ public class UserRepositoryImpl implements UserRepository {
 
     @Override
     public void createUser(User user) {
-
+        User userobject = new User();
+        mongoTemplate.insert(user);
     }
 
     @Override
     public void updateExpenses(String username, Expense expense) {
+//        User userExpenseUpdate = mongoTemplate.findOne(query(where("username").is(username)),User.class);
+//        List<Expense> expenseList = mongoTemplate.find(
+//                new Query(Criteria.where("username").is(userExpenseUpdate)),
+//                Expense.class);
+//        expenseList.add(expense);
 
+//        mongoTemplate.updateFirst(Query.query(Criteria.where("username")
+//                .is(username)), new Update().push("expense",expense), "users");
+
+        mongoTemplate.updateFirst(Query.query(Criteria.where("username")
+                .is(username)), new Update().addToSet("expenses",expense),"users");
     }
 
     @Override
--- src/test/java/com/lyomann/budgettracker/repository/UserRepositoryIntegrationTest.java
+++ src/test/java/com/lyomann/budgettracker/repository/UserRepositoryIntegrationTest.java
@@ -1,6 +1,8 @@
 package com.lyomann.budgettracker.repository;
 
+import com.lyomann.budgettracker.document.Expense;
 import com.lyomann.budgettracker.document.User;
+import com.mongodb.DBObject;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -10,7 +12,12 @@ import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import java.time.LocalDate;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Optional;
+
+import static com.lyomann.budgettracker.document.Category.*;
 
 @ExtendWith(SpringExtension.class)
 @DataMongoTest
@@ -21,24 +28,45 @@ public class UserRepositoryIntegrationTest {
 
     private UserRepository userRepository;
 
+
     User user = User.builder().username("Ly").build();
 
+    Expense expense1 = new Expense(100000L, LocalDate.of(2017, 11, 6),26.00,RENT,"Rent pay");
+    Expense expense2 = new Expense(100001L, LocalDate.of(2017, 11, 7),29.00,OTHER,"Entertainment");
+    List<Expense> expenseList = Arrays.asList(expense1, expense2);
+
+    User user1 = User.builder().id("1234").expenses(expenseList).username("Ty").build();
 
     @BeforeEach
     void setUp() {
         userRepository = new UserRepositoryImpl(mongoTemplate);
 
-
         mongoTemplate.insert(user, "users");
+        mongoTemplate.insert(user1, "users");
+
 
     }
 
     @Test
     void findUserByUsername_retrievesUserFromTheDatabase() {
+        mongoTemplate.insert(user, "users");
         Optional<User> ly = userRepository.findUserByUsername("Ly");
         Assertions.assertThat(ly.isPresent()).isTrue();
         Assertions.assertThat(ly.get()).isEqualTo(user);
 
 
+    }
+
+    @Test
+    void createNewUserInDatabase() {
+        mongoTemplate.insert(user1, "users");
+        Assertions.assertThat(mongoTemplate.findAll(DBObject.class, "users")).isNotEmpty();
+    }
+
+    @Test
+    void findUserByUsername_andUpdateListOfExpenses() {
+        Expense expense3 = new Expense(100002L, LocalDate.of(2017, 11, 9),69.00,SHOPPING,"Appliances");
+        userRepository.updateExpenses("Ty", expense3);
+        Assertions.assertThat(mongoTemplate.findAll(DBObject.class, "users")).isNotEmpty();
     }
 }
