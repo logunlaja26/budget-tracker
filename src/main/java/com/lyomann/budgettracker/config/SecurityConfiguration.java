9fd83cc48630de25450792858435916022761f26
Steven Diamante <SDiamante13@gmail.com>
2020-06-06 01:20:37 -0500

    Disable Spring Security for local testing. Since there is no way to pass Id tokens at the moment.

--- src/main/java/com/lyomann/budgettracker/config/SecurityConfiguration.java
+++ src/main/java/com/lyomann/budgettracker/config/SecurityConfiguration.java
@@ -3,12 +3,14 @@ package com.lyomann.budgettracker.config;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 
 @Configuration
+@Profile("prod")
 @RequiredArgsConstructor
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
